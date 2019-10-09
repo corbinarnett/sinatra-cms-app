8e9e0aee2d4aba1ef9ffebaae76c2a3feae52565
corbinarnett <corbin.c.arnett@gmail.com>
2019-10-09 15:53:29 -0700

    delete destination migration and model

--- db/migrate/20191009214926_create_destinations.rb
+++
@@ -1,8 +0,0 @@
-class CreateDestinations < ActiveRecord::Migration
-  def change
-    create_table :destinations do |t|
-      t.string :city
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20191009215804_create_dining_spots.rb
+++ db/migrate/20191009215804_create_dining_spots.rb
@@ -4,6 +4,7 @@ class CreateDiningSpots < ActiveRecord::Migration
       t.string :name
       t.string :address
       t.string :website
+      t.integer :user_id
       t.timestamps null: false
     end
   end
