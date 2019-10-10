34d5554aa9698ff48d735e2c6eb558e22582a5a3
corbinarnett <corbin.c.arnett@gmail.com>
2019-10-09 20:10:25 -0700

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController #allows the use of methods used in application controller
   
-  get '/user/login' do #RENDER LOGIN FORM
-    erb :"user/login"
+  get '/login' do #RENDER LOGIN FORM
+    erb :login
   end
 
-  get '/user/signup' do
+  post '/login' do 
+    @user = User.find_by(email: params[:email]) #find_by expects a key, value pair
 
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id #logs user in
+      redirect "users/#{@user.id}"
+    else
+
+    end
+  end
+
+  get '/signup' do
+
+  end
+
+  get '/users/:id' do #user SHOW 
+    "show user page"
   end
 
 end
