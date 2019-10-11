e90288672fe79bef6f8cc4476983ac8e046200d7
corbinarnett <corbin.c.arnett@gmail.com>
2019-10-10 17:47:20 -0700

    create post method for dining_spot form

--- app/controllers/dining_spots_controller.rb
+++ app/controllers/dining_spots_controller.rb
@@ -5,6 +5,17 @@ class DiningSpotsController < ApplicationController
   end
 
   post '/dining_spots' do
-    
+    if !logged_in?
+      redirect '/'
+    end
+
+    if params[:name] != "" && params[:address] != "" && params[:website] != ""
+      @dining_spot = DiningSpot.create(name: params[:name], address: params[:address], website: params[:website], user_id: current_user.id)
+      redirect "/dining_spots/#{@dining_spot.id}"
+    else
+      redirect "/dining_spots/new"
+    end
   end
+
+
 end
